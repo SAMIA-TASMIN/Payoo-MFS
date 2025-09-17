c92232663963a46f64a54491c9cc2f2908404802
SAMIA-TASMIN <novaira489@gmail.com>
2025-09-17 20:08:13 +0600

    commit

--- home.js
+++ home.js
@@ -1,11 +1,22 @@
+const validPin = 1234;
 document.getElementById('add-money-btn').addEventListener('click',function(e){
     e.preventDefault()
     const bank = document.getElementById('bank').value
     const accountNumber = document.getElementById('account-number').value
     const amount = parseInt(document.getElementById('add-amount').value)
-    const pin = document.getElementById('add-pin').value
+    const pin = parseInt(document.getElementById('add-pin').value)
     const availableBalance = parseInt(document.getElementById('available-bal').innerText)
-    console.log(availableBalance)
+    
+    if(accountNumber.length<11){
+        alert("Invalid account number")
+        return;
+    }
+
+    if(pin !== validPin){
+        alert("Invalid pin number")
+        return;
+    }
+
     const totalNewBalance = amount + availableBalance 
     document.getElementById('available-bal').innerText = totalNewBalance
 
