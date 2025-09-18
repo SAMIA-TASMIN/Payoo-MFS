402f13b45485ef5fcd48cce251d0c6988926d9c2
SAMIA-TASMIN <novaira489@gmail.com>
2025-09-18 15:32:26 +0600

    commit

--- home.js
+++ home.js
@@ -1,11 +1,34 @@
 const validPin = 1234;
+
+function getInputValueNumber(id){
+    const inputFieldValue = parseInt(document.getElementById(id).value)
+    return inputFieldValue
+}
+
+function getInputValue (id){
+    const inputField = document.getElementById(id).value
+    return inputField
+}
+
+function getInnerText(id){
+    const element = parseInt(document.getElementById(id).innerText)
+    return element
+
+}
+
+function setInnerText(value){
+    const availableBalanceElement = document.getElementById("available-bal")
+    availableBalanceElement.innerText = value
+}
+
+
 document.getElementById('add-money-btn').addEventListener('click',function(e){
     e.preventDefault()
-    const bank = document.getElementById('bank').value
-    const accountNumber = document.getElementById('account-number').value
-    const amount = parseInt(document.getElementById('add-amount').value)
-    const pin = parseInt(document.getElementById('add-pin').value)
-    const availableBalance = parseInt(document.getElementById('available-bal').innerText)
+    const bank = getInputValue('bank')
+    const accountNumber = getInputValue('account-number')
+    const amount = getInputValueNumber('add-amount')
+    const pin = getInputValueNumber('add-pin')
+    const availableBalance = getInnerText('available-bal')
     
     if(accountNumber.length<11){
         alert("Invalid account number")
@@ -18,15 +41,80 @@ document.getElementById('add-money-btn').addEventListener('click',function(e){
     }
 
     const totalNewBalance = amount + availableBalance 
-    document.getElementById('available-bal').innerText = totalNewBalance
+    setInnerText(totalNewBalance)
 
 })
+
+document.getElementById("withdraw-btn").addEventListener('click',function(e){
+    e.preventDefault()
+
+    const amount = getInputValueNumber("withdraw-amount")
+    const availableBalance = getInnerText("available-bal")
+
+    const totalNewBalance = availableBalance - amount
+    console.log(totalNewBalance)
+
+    setInnerText(totalNewBalance)
+})
+
 
 document.getElementById("add-botten").addEventListener('click',function(){
-    document.getElementById('cash-out-section').style.display = "none"
+    const forms = document.getElementsByClassName("form")
+
+    for(const form of forms)
+    {
+        form.style.display = "none"
+    }
     document.getElementById("add-money-section").style.display = "block"
 })
+
+
 document.getElementById("cashout-button").addEventListener('click',function(){
-    document.getElementById("add-money-section").style.display = "none"
-    document.getElementById('cash-out-section').style.display = "block"
+    const forms = document.getElementsByClassName("form")
+
+    for(const form of forms)
+    {
+        form.style.display = "none"
+    }
+    document.getElementById("cash-out-section").style.display = "block"
+})
+
+document.getElementById("transfer-money-btn").addEventListener('click',function(){
+    const forms = document.getElementsByClassName("form")
+
+    for(const form of forms)
+    {
+        form.style.display = "none"
+    }
+    document.getElementById("transfer-money-section").style.display = "block"
+})
+
+document.getElementById("get-bonus-btn").addEventListener('click',function(){
+    const forms = document.getElementsByClassName("form")
+
+    for(const form of forms)
+    {
+        form.style.display = "none"
+    }
+    document.getElementById("get-bonus-section").style.display = "block"
+})
+
+document.getElementById("pay-bill-btn").addEventListener('click',function(){
+    const forms = document.getElementsByClassName("form")
+
+    for(const form of forms)
+    {
+        form.style.display = "none"
+    }
+    document.getElementById("pay-bill-section").style.display = "block"
+})
+
+document.getElementById("transaction-btn").addEventListener('click',function(){
+    const forms = document.getElementsByClassName("form")
+
+    for(const form of forms)
+    {
+        form.style.display = "none"
+    }
+    document.getElementById("transaction-section").style.display = "block"
 })
